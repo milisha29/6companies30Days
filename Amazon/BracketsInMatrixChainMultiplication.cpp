4c56296599e400959e05bba81cc035f0192c1855
Milisha Gupta <86721067+milisha29@users.noreply.github.com>
2022-01-11 20:42:15 +0530

    Create BracketsInMatrixChainMultiplication.cpp

---
+++ Amazon/BracketsInMatrixChainMultiplication.cpp
@@ -0,0 +1,39 @@
+  string matrixChainOrder(int p[], int n)
+    {
+        
+        
+         vector<vector<pair<int,string>>> dp(n-1, vector<pair<int,string>>(n-1));
+
+        for(int i = 0; i < n-1; ++i)
+        {
+            string M = "";
+            M += (char) ('A' + i);
+            dp[i][i] = {0,M} ;
+        }
+       
+        for(int size = 1; size <= n; ++size) 
+        {
+            int l = 0;
+            int m = size;
+            while(m < n-1) 
+            {
+                dp[l][m].first = numeric_limits<int>::max();
+                
+                for(int i = l; i < m; ++i) 
+                {
+                    int operations = dp[l][i].first + dp[i+1][m].first + p[l]*p[i+1]*p[m+1];
+
+                    if(dp[l][m].first > operations) 
+                    {
+                        dp[l][m].second = "(" + dp[l][i].second + dp[i+1][m].second + ")";
+                        dp[l][m].first = operations;
+                    }
+                    
+                }
+
+                ++l;
+                ++m;
+            }
+        }
+        return dp[0].back().second ;
+    }
