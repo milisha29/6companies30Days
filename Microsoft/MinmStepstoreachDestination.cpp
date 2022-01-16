707d3e3325529baf78423dff0ca7d7edad6fe080
Milisha Gupta <86721067+milisha29@users.noreply.github.com>
2022-01-16 14:24:23 +0530

    Q14 done

---
+++ Microsoft/MinmStepstoreachDestination.cpp
@@ -0,0 +1,20 @@
+class Solution{
+public:
+    int minSteps(int D){
+        // code here
+        int steps = 0, sum = 0;
+        while(true){
+            sum = sum + steps;
+            steps++;
+            if(sum == D){
+                return steps-1;
+                break;
+            }
+            if(sum > D and (sum-D)%2 == 0){
+                return steps-1;
+                break;
+            }
+        }
+        return 0;
+    }
+};
