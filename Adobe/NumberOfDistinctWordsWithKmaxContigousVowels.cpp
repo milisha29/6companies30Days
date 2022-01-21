cf3f3f147612b28930b7655f62a3cf7143df07f0
Milisha Gupta <86721067+milisha29@users.noreply.github.com>
2022-01-21 22:43:37 +0530

    Q3

---
+++ Adobe/NumberOfDistinctWordsWithKmaxContigousVowels.cpp
@@ -0,0 +1,32 @@
+class Solution
+{
+  public:
+  
+  #define ll long long 
+  #define mod (ll) (1e9 +7)
+  
+  
+    int kvowelwords(int N, int K) {
+        vector<vector<ll>> dp (N+1 , vector<ll> (K+1 , 0)) ;
+        
+        for(int i=0;i<=N ; i++)
+        {
+            for(int j= 0 ; j<=K ; ++j)
+            {
+                
+                if(i==0) {  // initial condition 
+                    dp[i][j] = 1 ;
+                }
+                else  {
+                    dp[i][j] = (dp[i-1][K] * 21) % mod ;
+                    
+                    if(j>0) {
+                        dp[i][j] = (dp[i][j] + dp[i-1][j-1] *5 % mod) % mod ;
+                    }
+                }
+            }
+        }
+        return dp[N][K] ;
+    }
+};
+
