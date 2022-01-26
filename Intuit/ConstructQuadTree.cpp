e42fc753101165b96a706261a465e80843239274
Milisha Gupta <86721067+milisha29@users.noreply.github.com>
2022-01-26 23:09:23 +0530

    Create ConstructQuadTree.cpp

---
+++ Intuit/ConstructQuadTree.cpp
@@ -0,0 +1,48 @@
+Node * solve(vector<vector<int>>& grid , int rowstart , int rowend ,int colstart , int colend , int factor)
+ 
+  {
+      int sum = 0;  
+      for(int i=rowstart ;  i <rowend ; i++)
+      {
+          for(int j = colstart ; j < colend ; j++) 
+          {
+              sum += grid[i][j] ;
+          }
+      }
+      
+      
+      Node * dp ;
+      
+      if(sum ==0) 
+          dp = new Node(false ,true) ;
+      
+      else if(sum == (factor * factor))
+          
+          dp = new Node(true , true) ;
+      
+      
+      else 
+      {
+            dp = new Node(false,false);
+            factor/=2 ;
+          
+            dp->topLeft = solve(grid,rowstart,rowstart+factor,colstart,colstart+factor,factor);
+          
+            dp->topRight = solve(grid,rowstart,rowstart+factor,colstart+factor,colend,factor);
+          
+            dp->bottomLeft = solve(grid,rowstart+factor,rowend,colstart,colstart+factor,factor);
+          
+            dp->bottomRight = solve(grid,rowstart+factor,rowend,colstart+factor,colend,factor);
+      }
+       return dp ;
+  }
+    
+    
+    
+    Node* construct(vector<vector<int>>& grid) {
+        int n = grid.size() ;
+        if(n==0) return NULL ;
+        Node* ans;
+        ans = solve(grid,0,n,0,n,n) ;
+        return ans;
+    }
