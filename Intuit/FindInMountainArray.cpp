b000c643484c30b0d818d17a1885fa28d52dd179
Milisha Gupta <86721067+milisha29@users.noreply.github.com>
2022-01-26 23:14:47 +0530

    Create FindInMountainArray.cpp

---
+++ Intuit/FindInMountainArray.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    int findInMountainArray(int target, MountainArray &Arr) 
+    {
+          int left = 0 ;
+          int right = Arr.length() - 1;
+        // Find the peak index
+        while (left < right)
+        {    
+            int mid = left + (right - left) / 2;
+            if (Arr.get(mid) < Arr.get(mid + 1))
+            {
+                left = mid + 1;
+            } 
+            else
+            { 
+                right = mid;
+            }
+        }
+        
+        
+        int peak = left;
+        // Binary search on increasing subarray
+        left = 0;
+        right = peak;
+        while (left <= right) 
+        {
+            int mid = left + (right - left) / 2;
+            if (Arr.get(mid) < target) 
+            {
+                left = mid + 1;
+            } 
+            else if (Arr.get(mid) > target) 
+            { 
+                right = mid - 1;
+            } 
+            else
+            {
+                return mid;
+            }
+        }
+        
+        // Binary search on decreasing subarray
+        left = peak;
+        right = Arr.length() - 1;
+        while (left <= right) 
+        {
+            int mid = left + (right - left) / 2;
+            if (Arr.get(mid) < target)
+            {
+                right = mid - 1;
+            } 
+            else if (Arr.get(mid) > target) 
+            { 
+                left = mid + 1;
+            }
+            else
+            {
+                return mid;
+            }
+        }
+        
+        
+        return -1;
+    }
+};
